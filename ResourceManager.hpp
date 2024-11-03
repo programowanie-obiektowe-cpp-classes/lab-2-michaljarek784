0f5b998eefbfe65edaeb2111d13e64123872c995
Michal Jarek <michal.jarek784@wp.pl>
2024-11-03 22:34:46 +0100

    Zmiany

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,23 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    public:
+    private:
+    std::unique_ptr<Resource> resource;// Wskaźnik do zasobu
+
+    public:
+    ResourceManager() : resource(std::make_unique<Resource>()) {} // konstruktor domyślny - inicjalizuje zasób przy pomocy konstruktora Resource
+
+    // Metoda get(), która wywołuje metodę get() obiektu Resource
+    double get() const {
+        return resource->get();
+    }
+
+    // Domyślny konstruktor kopiujący i operator przypisania są usuwane, aby uniknąć kopiowania zasobu, który jest drogi w konstrukcji.
+    ResourceManager(const ResourceManager&) = delete;
+    ResourceManager& operator=(const ResourceManager&) = delete;
+
+    // Konstruktor przenoszący i operator przypisania przenoszącego
+    ResourceManager(ResourceManager&&) = default;
+    ResourceManager& operator=(ResourceManager&&) = default;
 };
